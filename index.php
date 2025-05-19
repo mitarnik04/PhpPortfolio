38a40892ce23401fff54ed895011f14a59356ff0
Mitar Nikolic <mitar.mn4@gmail.com>
2025-05-20 01:12:51 +0200

    remove testing code. Restructure

--- index.php
+++ index.php
@@ -6,15 +6,12 @@ define('DIR_VALIDATORS', __DIR__ . '/validators');
 require_once DIR_HELPERS . '/instance-provider.php';
 require_once DIR_HELPERS . '/router.php';
 require_once DIR_HELPERS . '/metadata.php';
+require_once DIR_HELPERS . '/translation.php';
 
 InstanceProvider::add(
     Router::class,
     Router::initialize(Metadata::getLoadableViews(), 'home', fn($pageName) => __DIR__ . '/views/' . $pageName . '.php')
 );
-
-class Something {}
-
-require_once DIR_HELPERS . '/translation.php';
 
 InstanceProvider::add(
     Translation::class,
